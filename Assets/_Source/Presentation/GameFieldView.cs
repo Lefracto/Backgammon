4d842d117c5d479c2e67bc2270c70890d5e08064
Anton Pauliuchyk <tauifyn@mail.ru>
2024-04-19 16:54:03 +0300

    Initial Unity commit.

---
+++ Assets/_Source/Presentation/GameFieldView.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFieldView : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
